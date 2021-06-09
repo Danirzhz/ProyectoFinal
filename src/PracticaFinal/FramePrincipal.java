d555773fce5200c96caf245d8e1cb9caff630c79
DanielRH <danirzhz@gmail.com>
2021-06-09 16:00:50 +0200

    Proyecto

--- src/PracticaFinal/FramePrincipal.java
+++ src/PracticaFinal/FramePrincipal.java
@@ -3,7 +3,7 @@ package PracticaFinal;
 import javax.swing.*;
 
 //Este será el frame en el que añadire todos los paneles
-import PracticaFinal.PanelAcciones;
+
 
 import java.awt.*;
 
@@ -16,13 +16,13 @@ public class FramePrincipal extends JPanel {
 
        JFrame framePrincipal=new JFrame();
         JPanel panelPrincipal= new JPanel(new GridLayout(1,3));
-        PanelAcciones panelAccion = new PanelAcciones();
+
         PanelFactura panelFac = new PanelFactura();
         PanelProductos panelPro = new PanelProductos();
 
         panelPrincipal.add(panelPro.componentesPanelPro());
         panelPrincipal.add(panelFac.componentesPanelFact());
-        panelPrincipal.add(panelAccion.componentesPanelAcciones());
+
 
 
 
